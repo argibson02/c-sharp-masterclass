bc00d85fe1d2b468c1e54b48cb3d3db91d7b1015
Alex Gibson <argibson02@gmail.com>
2022-06-07 13:34:15 -0500

    Section 10 part 2

---
+++ section10/abstraction/abstraction/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace abstraction
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Sphere s1 = new Sphere(4.21);
+            s1.GetInfo();
+
+
+            Shape[] shapes = { new Sphere(4), new Cube(9) };
+
+            foreach (Shape shape in shapes)
+            {
+                shape.GetInfo();
+                Console.WriteLine($"{shape.Name} has a volume of {shape.Volume()}");
+                Cube iceCube = shape as Cube;
+
+                if (iceCube == null)
+                {
+                    Console.WriteLine("This shape is not a cube.");
+                } 
+
+                if(shape is Cube) // checks type
+                {
+                    Console.WriteLine("This shape is a cube.");
+                }
+
+
+
+                object cube1 = new Cube(7);
+                Cube cube2 = (Cube)cube1;
+
+                Console.WriteLine($"{cube2.Name} has a v of {cube2.Volume()}");
+
+            }
+
+
+        }
+    }
+}
---
+++ section10/abstraction/abstraction/Sphere.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abstraction
+{
+    internal class Sphere : Shape
+    {
+
+        public double Diameter { get; set; }
+
+        public Sphere(double diameter)
+        {
+            Name = "Sphere";
+            this.Diameter = diameter;
+        }
+        public override double Volume()
+        {
+            return Math.Pow(Diameter / 2, 3) * (Math.PI * (4/3));
+        }
+
+        public override void GetInfo()
+        {
+            base.GetInfo();
+            Console.WriteLine($"tHE CUBE HAS A volume OF {Volume()}");
+        }
+
+    }
+}
